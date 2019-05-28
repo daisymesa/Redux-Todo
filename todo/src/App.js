68e8115c5eef435f926e8e4b3229f7f7fec9310e
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-28 19:18:57 -0400

    Added To do list components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
+      <div>
       <h1>App Component</h1>
+      <TodoList />
+      </div>
     </div>
   );
 }
---
+++ todo/src/components/Todos.js
@@ -0,0 +1,20 @@
+const Todos = [
+    {
+        value: 'Walk the dog.',
+        completed: false
+    },
+    {
+        value: 'Understand Redux.',
+        completed: false
+    },
+    {
+        value: 'Complete Redux Counter Project.',
+        completed: true
+    },
+    {
+        value: 'Complete Redux To Do App.',
+        completed: false
+    }
+]
+
+export default Todos
