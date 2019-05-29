36836a51f1c1247fccf96449872fa050b2ac5382
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-05-29 12:35:02 -0400

    Added addToDo component and set up store in index.js

--- todo-2/src/index.js
+++ todo-2/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'react-redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//Set up createStore function to receive the reducer
+const store = createStore(App)
+
+//Wrapping main React component inside of Provider tags
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
